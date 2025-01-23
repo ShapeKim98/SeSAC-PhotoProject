2b856131a8d978f66a9b4000437a9515128726b2
김도형 <shapekim98@gmail.com>
2025-01-23 19:08:04 +0900

    [feat] #13 저장하기 기능 구현

--- PhotoProject/PhotoProject/Feature/Profile/ProfileViewController.swift
+++ PhotoProject/PhotoProject/Feature/Profile/ProfileViewController.swift
@@ -13,10 +13,27 @@ class ProfileViewController: UIViewController {
     private let nicknameButton = UIButton()
     private let birthdayButton = UIButton()
     private let levelButton = UIButton()
+    private let saveButton = UIButton()
     
     private let nicknameLabel = UILabel()
     private let birthdayLabel = UILabel()
     private let levelLabel = UILabel()
+    
+    @UserDefaults(
+        forKey: .userDefaults(.nickname),
+        defaultValue: "NO NAME"
+    )
+    private var nickname: String?
+    @UserDefaults(
+        forKey: .userDefaults(.birthday),
+        defaultValue: "NO DATE"
+    )
+    private var birthday: String?
+    @UserDefaults(
+        forKey: .userDefaults(.level),
+        defaultValue: "NO LEVEL"
+    )
+    private var level: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +59,7 @@ class ProfileViewController: UIViewController {
         view.addSubview(nicknameButton)
         view.addSubview(birthdayButton)
         view.addSubview(levelButton)
+        view.addSubview(saveButton)
         
         view.addSubview(nicknameLabel)
         view.addSubview(birthdayLabel)
@@ -65,6 +83,11 @@ class ProfileViewController: UIViewController {
             make.top.equalTo(birthdayButton.snp.bottom).offset(24)
             make.height.equalTo(50)
             make.width.equalTo(100)
+        }
+        
+        saveButton.snp.makeConstraints { make in
+            make.bottom.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(50)
         }
         
         nicknameLabel.snp.makeConstraints { make in
@@ -91,10 +114,14 @@ class ProfileViewController: UIViewController {
         nicknameButton.setTitleColor(.black, for: .normal)
         birthdayButton.setTitleColor(.black, for: .normal)
         levelButton.setTitleColor(.black, for: .normal)
+        saveButton.setTitleColor(.white, for: .normal)
         
         nicknameButton.setTitle("닉네임", for: .normal)
         birthdayButton.setTitle("생일", for: .normal)
         levelButton.setTitle("레벨", for: .normal)
+        saveButton.setTitle("시작하기", for: .normal)
+        
+        saveButton.layer.cornerRadius = 25
         
         nicknameButton.addTarget(
             self,
@@ -111,18 +138,25 @@ class ProfileViewController: UIViewController {
             action: #selector(levelButtonTouchUpInside),
             for: .touchUpInside
         )
+        saveButton.addTarget(
+            self,
+            action: #selector(saveButtonTouchUpInside),
+            for: .touchUpInside
+        )
 
-        nicknameLabel.text = "NO NAME"
+        nicknameLabel.text = nickname
         nicknameLabel.textColor = .lightGray
         nicknameLabel.textAlignment = .right
         
-        birthdayLabel.text = "NO DATE"
+        birthdayLabel.text = birthday
         birthdayLabel.textColor = .lightGray
         birthdayLabel.textAlignment = .right
         
-        levelLabel.text = "NO LEVEL"
+        levelLabel.text = level
         levelLabel.textColor = .lightGray
         levelLabel.textAlignment = .right
+        
+        setSaveButtonState()
     }
 }
 
@@ -156,6 +190,16 @@ private extension ProfileViewController {
     func sinkBirthDayPublisher(_ notification: Notification) {
         let value = notification.userInfo?["birthday"] as? String
         birthdayLabel.text = value
+        setSaveButtonState()
+    }
+    
+    @objc
+    func saveButtonTouchUpInside() {
+        nickname = nicknameLabel.text
+        birthday = birthdayLabel.text
+        level = levelLabel.text
+        
+        presentAlert(title: "저장되었습니다.")
     }
     
     func scopeNicknameViewController(
@@ -165,12 +209,26 @@ private extension ProfileViewController {
         case .okButtonTapped(let nickname):
             let isEmpty = nickname?.isEmpty ?? true
             nicknameLabel.text = isEmpty ? "NO NAME" : nickname
+            setSaveButtonState()
         }
+    }
+    
+    func setSaveButtonState() {
+        let isNoName = nicknameLabel.text?.isEmpty ?? true
+        || nicknameLabel.text == "NO NAME"
+        let isNoDate = birthdayLabel.text?.isEmpty ?? true
+        || birthdayLabel.text == "NO DATE"
+        let isNoLevel = levelLabel.text?.isEmpty ?? true
+        || levelLabel.text == "NO LEVEL"
+        let isDisable = isNoName || isNoDate || isNoLevel
+        saveButton.backgroundColor = isDisable ? .secondaryLabel : .systemBlue
+        saveButton.isEnabled = !isDisable
     }
 }
 
 extension ProfileViewController: LevelViewControllerDelegate {
     func okLevelButtonTapped(_ level: String?) {
         levelLabel.text = level
+        setSaveButtonState()
     }
 }
---
+++ PhotoProject/PhotoProject/Util/UserDefaults/UserDefaults.swift
@@ -0,0 +1,45 @@
+//
+//  UserDefaults.swift
+//  PhotoProject
+//
+//  Created by 김도형 on 1/23/25.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefaults<T> {
+    let key: String
+    let defaultValue: T?
+    
+    init(forKey: String, defaultValue: T? = nil) {
+        self.key = forKey
+        self.defaultValue = defaultValue
+        if self.defaultValue == nil {
+            Foundation.UserDefaults.standard.set(defaultValue, forKey: key)
+        }
+    }
+    
+    var wrappedValue: T? {
+        get { (Foundation.UserDefaults.standard.object(forKey: key) as? T) ?? defaultValue }
+        set {
+            if newValue == nil {
+                Foundation.UserDefaults.standard.removeObject(forKey: key)
+            } else {
+                Foundation.UserDefaults.standard.set(newValue, forKey: key)
+            }
+        }
+    }
+}
+
+enum UserDefaultsKey: String {
+    case nickname = "Nickname"
+    case birthday = "Birthday"
+    case level = "Level"
+}
+
+extension String {
+    static func userDefaults(_ key: UserDefaultsKey) -> String {
+        key.rawValue
+    }
+}
